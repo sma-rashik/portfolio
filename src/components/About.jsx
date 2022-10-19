e86abee5add0a9f5579c23b03dacf9125c4e17e1
Rashik <smarashik00@gmail.com>
2022-10-19 22:42:32 +0600

    project-done

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          I'm a Front Web Developer. I am well experienced on HTML, CSS,
+          Bootstrap, Tailwind, Javascript & React Js. I have done many PSD to
+          HTML projects. Which was helped me to more attentive in wev
+          development. Minimum 1git commit -an 'edit file index.html'0 hours I
+          spent time code. Besides coding I have skilled on CMS like WordPress,
+          Wix, Shopify. I have also done many projects via CMS.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          I am an EEE student but my passion is to do code.When looking for
+          various information on the computer, what comes to mind are that, how
+          do we actually see the pages we are looking at? From that start
+          leaning towards to the web development. You can hire me to do work
+          with you.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-full bg-gradient-to-b from-black to-gray-800 p-4  text-white pt-20"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me</p>
+        </div>
+
+        <div className=" flex justify-center items-center">
+          <form
+            action="https://getform.io/f/99328f6d-d1e4-4e1d-a392-ce352872b5e9"
+            method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter your email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's talk
+            </button>
+          </form>
+        </div>
+      </div>
+      <p className="text-right text-gray ">
+        All right reserved By : &copy;{" "}
+        <a
+          href="https://www.facebook.com/sma_rashik"
+          className="font-bold border-b-4 border-gray-500"
+          target="_blank"
+          rel="noreferrer"
+        >
+          S M ABDUL RASHIK{" "}
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
